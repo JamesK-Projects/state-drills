e5828b62dcf2092cef2266442575c2c93c506a57
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-09-14 11:21:21 -0600

    updated for Accordion component

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './state-drills/Accordion';
 
 class App extends Component{
+  
   render() {
     return(
       <div>
-        <HelloWorld />
-        <Bomb />
-        <RouletteGun />
+        <Accordion />
       </div>
     )
   }
 }
-
 
 export default App;
